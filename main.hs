d888e1250d1c1c4eb99412e3e1abeca65fbbdf86
Luke Iannini <lukexi@me.com>
2015-03-24 04:25:34 -0700

    Refactoring and comments

--- main.hs
+++ main.hs
@@ -9,6 +9,7 @@ import Foreign.Storable (sizeOf)
 -- Initialization to set up window --
 main :: IO ()
 main = do
+  -- Initialize GLFW
   _ <- init
   applyWindowHints
   
@@ -19,10 +20,13 @@ main = do
     Just window -> do
       -- Enable VSync
       swapInterval 1
+
       makeContextCurrent ( Just window )
 
-      shader <- initResources
+      -- Create and bind our shader + a VAO holding the fullscreen quad geometry
+      shader <- setupResources
 
+      -- Begin rendering
       mainLoop window shader 0
 
 -- Actually Starting once window is up --
@@ -40,7 +44,7 @@ mainLoop window shader frameNumber = do
   uniform (spsFrameNumberU shader) $= Index1 frameNumber
   
   -- Draw the fullscreens quad
-  drawElements TriangleStrip (fromIntegral (length elementBufferData)) UnsignedInt offset0
+  drawElements TriangleStrip elementLength UnsignedInt offset0
 
   -- Swap buffers, poll events, and start rendering the next frame
   swapBuffers window
@@ -62,10 +66,25 @@ vertexBufferData =  [ -1 , -1
                     , -1 , 1 
                     , 1  , 1 ]
 
+-- Describe the vertex layout
+vertexDescriptor :: VertexArrayDescriptor a
+vertexDescriptor = VertexArrayDescriptor vertexComponents Float vertexStride offset0
+  where
+    -- We only use X & Y for a fullscreen quad
+    vertexComponents :: Num a => a
+    vertexComponents = 2
+
+    vertexStride :: Num a => a
+    vertexStride = vertexComponents * fromIntegral (sizeOf (undefined::GLfloat))
+
 -- Indices to render a fullscreen quad as a TriangleStrip
 elementBufferData :: [ GLuint ]
 elementBufferData =  [ 0..3   ]
 
+elementLength :: NumArrayIndices
+elementLength = fromIntegral (length elementBufferData)
+
+-- | Load our shaders and find their attribute/uniform locations
 initShader :: IO SPShader
 initShader = do 
   vs <- loadShader VertexShader   "spacepuppy.vert"
@@ -75,22 +94,20 @@ initShader = do
     <$> get (attribLocation p "position")
     <*> get (uniformLocation p "frameNumber")
 
-initResources :: IO SPShader
-initResources = do
+setupResources :: IO SPShader
+setupResources = do
   shader   <- initShader
   vertices <- makeBuffer ArrayBuffer vertexBufferData
   indices  <- makeBuffer ElementArrayBuffer elementBufferData
 
-  -- Create a VAO, as required by the OpenGL Core Context
+  -- Create a VAO for our geometry, as required by the OpenGL Core Context
   vao <- makeVAO $ do
-    let numComponents = 2 -- We only use X & Y since this is a fullscreen quad
-        stride    = fromIntegral $ sizeOf (undefined::GLfloat) * fromIntegral numComponents
-        vad       = VertexArrayDescriptor numComponents Float stride offset0
-        positionA = spsPositionA shader
-    -- Bind our vertices
+    let positionA = spsPositionA shader
+    -- Bind the vertex buffer
     bindBuffer ArrayBuffer        $= Just vertices
-    vertexAttribArray positionA   $= Enabled
-    vertexAttribPointer positionA $= (ToFloat, vad)
+    -- Indicate where and how to provide the vertexes
+    vertexAttribArray   positionA $= Enabled
+    vertexAttribPointer positionA $= (ToFloat, vertexDescriptor)
     -- Bind the indices to render the quad as a triangle strip
     bindBuffer ElementArrayBuffer $= Just indices
 
@@ -102,6 +119,7 @@ initResources = do
 
   return shader
 
+-- Window hints required for using OpenGL on Mac
 applyWindowHints :: IO ()
 applyWindowHints = do
   windowHint $ WindowHint'ClientAPI ClientAPI'OpenGL
